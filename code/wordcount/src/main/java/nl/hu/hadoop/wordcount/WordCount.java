b00b1415c79710b1f573f0d5ef1ddbb219adb453
dannyhorvath1992 <danny.horvath@student.hu.nl>
2016-03-01 10:45:36 +0100

    Faster divisor function
    Faster divisor function

--- code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
+++ code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
@@ -43,10 +43,16 @@ class WordCountMapper extends Mapper<LongWritable, Text, IntWritable, IntWritabl
 	}
 
 	public int divisors(int n){
-		int sum = 0;
-		for(int i = 1; i < (n/2+1); i++){
-			if(n % i == 0){
+		int maxD = (int)Math.sqrt(n);
+		int sum=1;
+		for(int i = 2; i <= maxD; i++)
+		{
+			if(n % i == 0)
+			{
 				sum += i;
+				int d = n/i;
+				if(d!=i)
+					sum+=d;
 			}
 		}
 		return sum;
